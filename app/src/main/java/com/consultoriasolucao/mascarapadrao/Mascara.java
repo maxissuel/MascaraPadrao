8ffb4d96c70a4bd1e60d27ab0acc9506a49fa2d9
maxissuel <mike.souza100@gmail.com>
2019-04-11 16:40:43 -0400

    Adicionada a mascara de moeda.

--- app/src/main/java/com/consultoriasolucao/mascarapadrao/Mascara.java
+++ app/src/main/java/com/consultoriasolucao/mascarapadrao/Mascara.java
@@ -4,6 +4,11 @@ import android.text.Editable;
 import android.text.TextWatcher;
 import android.widget.EditText;
 
+import java.lang.ref.WeakReference;
+import java.math.BigDecimal;
+import java.text.NumberFormat;
+import java.util.Locale;
+
 public class Mascara {
     public static final String CPF = "###.###.###-##";
     public static final String CNPJ = "##.###.###/####-##";
@@ -37,7 +42,7 @@ public class Mascara {
         return mascara;
     }
 
-    public static TextWatcher insert(final String maskPadrao, final EditText ediTxt) {
+    public static TextWatcher insereMascara(final String maskPadrao, final EditText ediTxt) {
         return new TextWatcher() {
             boolean isUpdating;
             String old = "";
@@ -45,9 +50,9 @@ public class Mascara {
             public void onTextChanged(CharSequence s, int start, int before,
                                       int count) {
                 String maskAtual = maskPadrao;
-                if(maskAtual.equals(CPF) || maskAtual.equals(CNPJ)){
+                if (maskAtual.equals(CPF) || maskAtual.equals(CNPJ)) {
                     String str = Mascara.retiraMascara(s.toString());
-                    if(str.length() <= 11)
+                    if (str.length() <= 11)
                         maskAtual = CPF;
                     else
                         maskAtual = CNPJ;
@@ -86,5 +91,53 @@ public class Mascara {
             public void afterTextChanged(Editable s) {
             }
         };
+    }
+
+    public static TextWatcher insereMascara(final EditText ediTxt, final Locale moedaCorrente) {
+        return new TextWatcherMoeda(ediTxt,moedaCorrente);
+    }
+
+    private static class TextWatcherMoeda implements TextWatcher {
+        private final WeakReference<EditText> editTextWeakReference;
+        private final Locale locale;
+
+        public TextWatcherMoeda(EditText editText, Locale locale) {
+            this.editTextWeakReference = new WeakReference<EditText>(editText);
+            this.locale = locale != null ? locale : Locale.getDefault();
+        }
+
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+        }
+
+        @Override
+        public void afterTextChanged(Editable editable) {
+            EditText editText = editTextWeakReference.get();
+            if (editText == null) return;
+            editText.removeTextChangedListener(this);
+
+            BigDecimal parsed = parseToBigDecimal(editable.toString(), locale);
+            String formatted = NumberFormat.getCurrencyInstance(locale).format(parsed);
+
+            editText.setText(formatted);
+            editText.setSelection(formatted.length());
+            editText.addTextChangedListener(this);
+        }
+
+        private BigDecimal parseToBigDecimal(String value, Locale locale) {
+            String replaceable = String.format("[%s,.\\s]", NumberFormat.getCurrencyInstance(locale).getCurrency().getSymbol());
+
+            String cleanString = value.replaceAll(replaceable, "");
+
+            return new BigDecimal(cleanString).setScale(
+                    2, BigDecimal.ROUND_FLOOR).divide(new BigDecimal(100), BigDecimal.ROUND_FLOOR
+            );
+        }
     }
 }
